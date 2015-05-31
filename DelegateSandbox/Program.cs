c3c2c5535d9ee1cd353133fc4aabe074f6baa629
jwbats <jwbats@gmail.com>
2015-05-31 18:41:03 +0200

    Initial commit.

---
+++ DelegateSandbox/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSandbox
+{
+	class Program
+	{
+
+		private delegate void DelegatePrintString(string str);
+
+
+		private static void MethodPrintStringLower(string str)
+		{
+			Console.WriteLine(str.ToLower());
+		}
+
+		private static void MethodPrintStringUpper(string str)
+		{
+			Console.WriteLine(str.ToUpper());
+		}
+
+
+		static void Main(string[] args)
+		{
+			DelegatePrintString delegatePrintString = null;
+
+			delegatePrintString += new DelegatePrintString(MethodPrintStringLower);
+			delegatePrintString += new DelegatePrintString(MethodPrintStringUpper);
+
+			delegatePrintString("Output");
+
+			Console.WriteLine();
+			Console.WriteLine();
+
+			Func<int> func = delegate { Console.WriteLine("first part"); return 5; };
+			func += delegate { Console.WriteLine("second part"); return 7; };
+			int result = func();
+			Console.WriteLine(result);
+
+			Console.WriteLine();
+			Console.WriteLine();
+
+			foreach (Func<int> part in func.GetInvocationList())
+			{
+				result = part();
+				Console.WriteLine(result);
+			}
+
+			Console.ReadKey();
+
+
+		}
+	}
+}
